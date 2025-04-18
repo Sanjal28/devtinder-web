f9043489f4869812699a7308a3a1a4591644e24c
Sanjal28 <slsanjal28@gmail.com>
2025-04-18 15:27:30 +0530

    accept/reject connection request

---
+++ src/components/Requests.jsx
@@ -0,0 +1,90 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { BASE_URL } from "../constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addRequests, removeRequest } from "../utils/requestSlice";
+
+const Requests = () => {
+  const dispatch = useDispatch();
+  const requests = useSelector((store) => store.requests);
+  const fetchRequests = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/requests/recieved", {
+        withCredentials: true,
+      });
+      dispatch(addRequests(res.data.data));
+    } catch (err) {
+      console.error("Error fetching requests:", err.message);
+    }
+  };
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+  const reviewRequest = async (status, _id) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/review/" + status + "/" + _id,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(removeRequest(_id));
+    } catch (err) {
+      console.error("Error reviewing request:", err.message);
+    }
+  };
+  if (!requests) return;
+  if (requests.length === 0)
+    return (
+      <div className="flex justify-center items-center h-[80vh] text-2xl font-bold">
+        No Connection Requests Found
+      </div>
+    );
+  return (
+    <div className="text-center my-10">
+      <h1 className="text-bold text-white text-3xl">Connection Requests</h1>
+      {requests.map((request) => {
+        const { _id, firstName, lastName, photoUrl, age, gender, about } =
+          request.fromUserId;
+        return (
+          <div
+            key={_id}
+            className="flex justify-between items-center m-4 p-4 rounded-lg bg-gray-700 w-1/3 mx-auto"
+          >
+            <div>
+              <img
+                alt="Photo"
+                className="w-20 h-20 rounded-full"
+                src={photoUrl}
+              />
+            </div>
+            <div className="text-left mx-4">
+              <h2 className="font-bold text-xl">
+                {firstName + " " + lastName}
+              </h2>
+              {age && gender && <p>{age + " , " + gender}</p>}
+              <p>{about}</p>
+            </div>
+            <div>
+              <button
+                className="btn btn-soft btn-accent mx-2"
+                onClick={() => reviewRequest("accepted", request._id)}
+              >
+                Accept
+              </button>
+              <button
+                className="btn btn-soft btn-secondary mx-2"
+                onClick={() => reviewRequest("rejected", request._id)}
+              >
+                Reject
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Requests;
